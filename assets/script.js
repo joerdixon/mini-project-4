71d5d21bceafbd05ef78d016b4ad25019ef35d2e
joerdixon <joseph35dixon@gmail.com>
2022-09-30 16:22:11 -0700

    almost done

--- assets/script.js
+++ assets/script.js
@@ -10,81 +10,138 @@ var lossCount = document.getElementById("losscount");
 // Wordspace
 var wordDisplay = document.getElementById("actualword");
 
+// Control Array
 var randWord;
+// Seen By Player Array
+var sbpArray;
 
+// Initialize Stats
+if (localStorage.getItem("wins") !== null) {
+    var wins = localStorage.getItem("wins")
+    renderCurrentWord();
+} else {
+    wins = 0;
+}
+
+if (localStorage.getItem("losses") !== null) {
+    var losses = localStorage.getItem("losses")
+    renderCurrentWord();
+} else {
+    wins = 0;
+}
 
 // FUNCTIONS ------------------------------------------
 
-// function getWord() {
-//     var randWord = wordlist[Math.floor(Math.random() * wordlist.length)]
-//     randWord = randWord.split("")
-//     return randWord;
-// }
-
 function startGame(event) {
+    // GENERATE A RANDOM WORD FROM THE LIST AND SPLIT INTO AN ARRAY
     randWord = wordlist[Math.floor(Math.random() * wordlist.length)];
     randWord = randWord.split("")
-    // console.log(randWord);
-
+    // RESET PLAYER ARRAY TO BE EMPTY
+    sbpArray = [];
+    // FOR THE LENGTH OF THE CONTROL ARRAY
     for (var i = 0; i < randWord.length; i++) {
+        // ADD UNDERSCORES
+        sbpArray.push("_");
+            // PRINT TO SCREEN
+        renderCurrentWord();
+    }
+
+}
+
+
+// WHEN THE PLAYER PRESSES A KEY
+function playerGuess(event) {
+    // FOR THE LENGTH OF THE CONTROL ARRAY
+    for (var i = 0; i < randWord.length; i++) {
+        // IF THE GUESS AND A LETTER MATCH
+        if (event.key == randWord[i]) {
+            // CHANGE THE UNDERSCORE TO THE REVEALED LETTER
+            sbpArray[i] = randWord[i];
+            // UPDATE DISPLAY
+            renderCurrentWord();
+        }
+    }
+    // WIN CONDITION
+    if (!sbpArray.includes("_")) {
+        win();
+    }
+} 
+
+// WHEN WE NEED TO UPDATE/INITIALIZE THE SCREEN
+function renderCurrentWord() {
+    winCount.textContent = "Wins: " + wins;
+    lossCount.textContent = "Losses: " + losses;
+    // ERASE EVERYTHING ALREADY THERE
+    wordDisplay.innerHTML = "";
+    if (randWord !== undefined) {
+            // FOR THE LENGTH OF THE CONTROL ARRAY
+            for (var i = 0; i < randWord.length; i++) {
+        // CREATE A P TAG
         var underScore = document.createElement("p");
-        underScore.textContent = "_";
-        underScore.setAttribute("id", "box" + i);
-        // console.log(underScore.getAttribute("data-index"));
+        // SET TEXT CONTENT TO THE CORRECT STATE
+        underScore.textContent = sbpArray[i];
+        // STYLE AND APPEND
         underScore.setAttribute("style", "font-size: 2rem; display: inline; margin: 10px; text-align: center;")
         wordDisplay.appendChild(underScore)
     }
-}
-
-function playerGuess(event) {
-//     var keyPressed = event.key.toLowerCase();
-//     var alphaNumeric = "abcdefghigklmnopqrstuvwxyz1234567890 ".split("");
-    // Check to see if alphaNumeric includes keyPressed.
-    console.log(randWord);
-
-    for (var i = 0; i < randWord.length; i++) {
-        if (event.key == randWord[i]) {
-            var index = wordDisplay.getAttribute("data-index");
-            wordDisplay.innerHTML = event.key;
-        }
     }
 
-    // console.log(event);
+
+}
+
+// WHEN THE PLAYER WINS
+function win() {
+    wordDisplay.innerHTML = "YOU WIN!!!"
+    wins++;
+    storeStats();
+    renderCurrentWord();
+}
+
+function loss() {
+    wordDisplay.innerHTML = "YOU LOSE!!!"
 } 
+
+
+function storeStats() {
+    localStorage.setItem("wins", wins);
+    localStorage.setItem("losses", losses);
+}
+
+function resetStats() {
+    localStorage.setItem("wins", 0);
+    localStorage.setItem("losses", 0);
+    wins = 0;
+    losses = 0;
+    renderCurrentWord();
+}
+
 
 // EVENT LISTENERS ------------------------------------
 
+
+// WHEN A USER HITS START
 startButton.addEventListener("click", startGame)
 
+
+// WHEN A USER HITS A KEYSTROKE
 document.addEventListener("keyup", playerGuess)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+// WHEN A USER HITS RESET STATS BUTTON
+statsReset.addEventListener("click", resetStats)
 
 
 /* 
-When the page is loaded, check to see if the user has a record.
+When the page is loaded, check to see if the user has a record. TODO:
 
-if so, render to the page in the wins and losses categories.
+if so, render to the page in the wins and losses categories. TODO:
 
-When the player hits the start button.
+When the player hits the start button. 
 
-Start the timer,
+Start the timer, TODO:
 Randomly select a string from wordlist.
 split the string into its own array.
 
-var controlArray = create variable of filled in array. - set to the side for comparison -
+var randWord = create variable of filled in array. - set to the side for comparison -
 
 For (each item in the array we made from wordlist)
 
